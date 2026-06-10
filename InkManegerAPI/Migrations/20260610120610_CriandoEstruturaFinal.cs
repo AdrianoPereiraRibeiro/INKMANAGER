9cc04d7e91cf3707e6cd5be5d43617454a9a5040
AdrianoPereiraRibeiro <adrianoribeiro291005@gmail.com>
2026-06-10 09:07:05 -0300

    configurando migrations banco de dados

---
+++ InkManegerAPI/Migrations/20260610120610_CriandoEstruturaFinal.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InkManegerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriandoEstruturaFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
